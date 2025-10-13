ce91d1e257b9cec310543df1750522325c8d71f8
KURO <ductoantran17@gmail.com>
2025-10-13 11:40:54 +0700

    fix(login): change logic

--- src/page/Login/LoginPage.tsx
+++ src/page/Login/LoginPage.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useContext } from "react";
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
 import { StateContext } from "../../Plugin/Context/Context";
+
 const LoginPage = () => {
     const context = useContext(StateContext);
     if (!context) return null;
