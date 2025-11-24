e7e829ff6f8615c69f3f1de82aefab91022d80bf
KURO <ductoantran17@gmail.com>
2025-11-24 16:59:39 +0700

    feacture: create form otp

--- src/page/Login/OTP.tsx
+++ src/page/Login/OTP.tsx
@@ -1,6 +1,38 @@
-const OTP = () => {
-    return(
-        <div></div>
-    )
+import { useState } from "react"
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "../../components/ui/input-otp"
+
+export default function OTP() {
+  const demoOTP = "123456"
+  const [otp, setOtp] = useState("")
+
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <InputOTP
+        maxLength={6}
+        value={otp}
+        onChange={setOtp}
+      >
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+        </InputOTPGroup>
+        <InputOTPSeparator />
+        <InputOTPGroup>
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOTP>
+
+      <p className="text-sm text-gray-600">
+        Bạn nhập: {otp || "Chưa nhập gì"}
+      </p>
+    </div>
+  )
 }
-export default OTP
